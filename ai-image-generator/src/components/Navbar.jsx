41baec3531ea205c4e8e7beff95685db45096931
patelSahil9 <ptulasi578@gmail.com>
2024-12-12 15:13:52 +0530

    .

---
+++ ai-image-generator/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './navbar.css'
+const Navbar = () => {
+
+    return (
+
+        <div className='nav' >
+            <h2>AI Image Generator</h2>
+            <div id='right'><h1>Pricing</h1>
+            <button>Login</button>
+            </div>
+        </div>
+
+    )
+}
+
+export default Navbar
+
+
