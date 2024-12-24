ed19b2347a2b2746ce539ed969a5977ed32cfd2e
Patel Sahil <155907046+patelSahil9@users.noreply.github.com>
2024-12-24 17:26:21 +0530

    Co-authored-by: Patel Het  <hetpatel2508@users.noreply.github.com>

--- ai-image-generator/client/src/App.jsx
+++ ai-image-generator/client/src/App.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
-import Front from './components/Front';
-import Login from './components/Login';
-
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Front from "./components/Front";
+import Login from "./components/Login";
+import {Route,BrowserRouter,Routes} from 'react-router-dom'
 export default function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Front />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Front />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
--- ai-image-generator/client/src/components/Navbar.jsx
+++ ai-image-generator/client/src/components/Navbar.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 
 const Navbar = () => {
   return (
-    <nav className="text-gray-800">
+    <nav className="bg-blue-600 text-gray-800">
       <div className="container mx-auto px-4 flex justify-between items-center h-16">
         <div className="text-xl font-bold">
-          <img src="/public/LOGO.png" alt="Logo" className="w-[140px] h-full" />
+          <img src="/public/LOGO.png" className='w-[140px] h-full' />
         </div>
 
         <div className="flex space-x-6">
-          <Link to="/pricing" className="w-[70px] h-[37px] flex justify-center items-center">
-            Pricing
-          </Link>
-          <Link
-            to="/login"
-            className="w-[70px] h-[37px] flex justify-center items-center hover:text-gray-300 hover:bg-gray-500 rounded-full transition ease-in-out duration-300"
-          >
-            Login
-          </Link>
+          <a href="/pricing" className="w-[70px] h-[37px] flex justify-center items-center ">Pricing</a>
+          <Link to="/login" className="w-[70px] h-[37px] flex justify-center items-center hover:text-gray-300 hover:bg-gray-500 rounded-full transition ease-in-out duration-300">Login</Link>
         </div>
       </div>
     </nav>
