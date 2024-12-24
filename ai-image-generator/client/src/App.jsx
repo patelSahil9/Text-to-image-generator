c124037cda0fd3e8f12a7af3b145b6aa896a005b
Patel Sahil <155907046+patelSahil9@users.noreply.github.com>
2024-12-24 13:37:08 +0530

    .

--- ai-image-generator/client/src/App.jsx
+++ ai-image-generator/client/src/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
-import Front from './components/Front'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Front from './components/Front';
+import Login from './components/Login';
+
 export default function App() {
   return (
-    <>
-    <Navbar/>
-    <Front/>
-    <div></div>
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Front />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
--- ai-image-generator/client/src/components/Navbar.jsx
+++ ai-image-generator/client/src/components/Navbar.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className=" text-gray-800">
+    <nav className="text-gray-800">
       <div className="container mx-auto px-4 flex justify-between items-center h-16">
         <div className="text-xl font-bold">
-          <img src="/public/LOGO.png" className='w-[140px] h-full' />
+          <img src="/public/LOGO.png" alt="Logo" className="w-[140px] h-full" />
         </div>
 
         <div className="flex space-x-6">
-          <a href="/pricing" className="w-[70px] h-[37px] flex justify-center items-center ">Pricing</a>
-          <a href="/login" className="w-[70px] h-[37px] flex justify-center items-center hover:text-gray-300 hover:bg-gray-500 rounded-full transition ease-in-out duration-300">Login</a>
+          <Link to="/pricing" className="w-[70px] h-[37px] flex justify-center items-center">
+            Pricing
+          </Link>
+          <Link
+            to="/login"
+            className="w-[70px] h-[37px] flex justify-center items-center hover:text-gray-300 hover:bg-gray-500 rounded-full transition ease-in-out duration-300"
+          >
+            Login
+          </Link>
         </div>
       </div>
     </nav>
@@ -18,4 +26,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
